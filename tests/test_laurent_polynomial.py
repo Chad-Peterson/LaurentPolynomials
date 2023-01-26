cb8c7987acb2839eeef126f7c2a56878469a57b8
Chad-Peterson <97306485+Chad-Peterson@users.noreply.github.com>
2023-01-25 23:09:28 -0600

    Fix project name format

---
+++ tests/test_laurent_polynomial.py
@@ -0,0 +1,27 @@
+from src.laurentpolynomials.laurent_polynomial import LaurentPolynomial
+
+# Test Class Creation
+
+
+# Test Addition
+
+def test_addition():
+    a = LaurentPolynomial('A')
+
+    assert a + a == 2 * a
+
+# Test Subtraction
+
+
+# Test Multiplication
+
+
+# Test Division
+
+
+# Test Exponentiation
+
+
+# Test Compound Arithmetic
+
+# Test Weird Stuff
